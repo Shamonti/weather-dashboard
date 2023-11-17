361190b6ab8b86b0d33103507070b2ada28dcebb
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-11-17 15:56:46 +0600

    Added TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='App'>
-      <h1>Weather Dashboard</h1>
+      <h1 className='text-3xl font-bold underline'>Weather Dashboard</h1>
     </div>
   );
 }

5f971b473e682dc81f5b9c490ce2596155f61860
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-11-18 19:45:41 +0600

    Fixed all responsiveness

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div className='ms-2 w-3/5'>
+    <div className='ms-2 w-3/5 md:w-3/4'>
       <input
         className='flex-initial w-full placeholder:italic placeholder:text-slate-400 p-2 ms-2 bg-slate-100 border border-slate-300 rounded-md shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
         type='text'
